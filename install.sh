27606fed837d1897b1cd9d460fd19c6ae7d2ceb5
alexjuro <alexander.lesnjak@gmail.com>
2024-03-01 00:13:03 +0100

    refactor: migrated alacritty to toml

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo pacman -Syu
 sudo pacman -S hyprland hyprpaper wofi nautilus alacritty waybar --noconfirm
-sudo pacman -S code discord chromium --noconfirm
+sudo pacman -S code discord chromium nerd-fonts starship --noconfirm
 
 cp -r ./config/* ~/.config
