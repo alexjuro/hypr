560abd75f86651206a41baf38a1c2daf9367f8d3
Alexander Lesnjak <alexander.lesnjak@gmail.com>
2024-10-26 14:16:40 +0200

    feat: added font-awesome

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #/bin/sh
 
 sudo pacman -Syu
-sudo pacman -Sy hyprland hyprpaper wofi nautilus alacritty waybar code chromium nerd-fonts starship --noconfirm
+sudo pacman -Sy hyprland hyprpaper wofi nautilus alacritty waybar code chromium nerd-fonts starship ttf-font-awesome --noconfirm
+
+chmod +x ./update.sh
