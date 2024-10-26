0a0d78edc946aca58d6be3d38060d5ed3bdd51f7
Alexander Lesnjak <alexander.lesnjak@gmail.com>
2024-10-26 16:05:42 +0200

    feat: added copy to inital install

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 sudo pacman -Syu
 sudo pacman -Sy hyprland hyprpaper wofi nautilus alacritty waybar code chromium nerd-fonts starship ttf-font-awesome --noconfirm
 
+cp -r ./config/* ~/.config
 chmod +x ./update.sh
