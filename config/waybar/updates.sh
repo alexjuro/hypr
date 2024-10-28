aeb8ce7497cc8ccbe3e84be2a6199bd359de1b8a
Alexander Lesnjak <alexander.lesnjak@gmail.com>
2024-10-28 13:44:28 +0100

    style: updated update symbol

--- config/waybar/updates.sh
+++ config/waybar/updates.sh
@@ -2,7 +2,7 @@
 updates=$(pacman -Qu | wc -l)
 
 if [ "$updates" -gt 3 ]; then
-    printf '{"text": "%s "}' "$updates"
+    printf '{"text": ""}'
 else
-    printf '{"text": "0 "}'
+    printf '{"text": ""}'
 fi
