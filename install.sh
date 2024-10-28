3c2c5dd00a0ed98e1f3d5636f9f95c85de8ef101
Alexander Lesnjak <alexander.lesnjak@gmail.com>
2024-10-28 13:44:05 +0100

    feat: removed font-awesome

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 sudo pacman -Syu
-sudo pacman -Sy hyprland hyprpaper wofi nautilus alacritty waybar code chromium nerd-fonts starship ttf-font-awesome --noconfirm
+sudo pacman -Sy hyprland hyprpaper wofi nautilus alacritty waybar code chromium nerd-fonts starship --noconfirm
 
 cp -r ./config/* ~/.config
 chmod +x ./update.sh
