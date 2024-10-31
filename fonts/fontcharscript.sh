7a2c8fde1f892f7cd27e4c0d2e433fe64e148cee
Alexander Lesnjak <alexander.lesnjak@gmail.com>
2024-10-31 12:21:53 +0100

    feat: filtred icons

--- fonts/fontcharscript.sh
+++ fonts/fontcharscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
-# utility to show all chars of a script
+# utility to show all chars of a script, limited to 2000 characters
 
 Usage() { echo "$0 FontFile"; exit 1; }
 SayError() { local error=$1; shift; echo "$0: $@"; exit "$error"; }
@@ -13,17 +13,30 @@ fontfile="$1"
 
 list=$(fc-query --format='%{charset}\n' "$fontfile")
 
-for    range in $list
-do     IFS=- read start end <<<"$range"
-       if    [ "$end" ]
-       then
-             start=$((16#$start))
-         end=$((16#$end))
-         for((i=start;i<=end;i++)); do
-         printf -v char '\\U%x' "$i"
-         printf '%b' "$char"
-         done
-       else
-         printf '%b' "\\U$start"
-       fi
-done | grep -oP '.{'"$width"'}'
+output=""
+char_count=0
+max_chars=30000
+
+for range in $list; do
+    IFS=- read start end <<<"$range"
+    if [ "$end" ]; then
+        start=$((16#$start))
+        end=$((16#$end))
+        for ((i=start; i<=end; i++)); do
+            printf -v char '\\U%x' "$i"
+            output+="$(printf '%b' "$char  ")"
+            char_count=${#output}
+            if [ "$char_count" -ge "$max_chars" ]; then
+                break 2
+            fi
+        done
+    else
+        output+="$(printf '%b' "\\U$start  ")"
+        char_count=${#output}
+        if [ "$char_count" -ge "$max_chars" ]; then
+            break
+        fi
+    fi
+done
+
+echo "${output:0:max_chars}" | grep -oP '.{'"$width"'}'
