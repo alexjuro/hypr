c3bfd62351fa4bfd66ec7ac48bdb4ec09840baf6
Alexander Lesnjak <alexander.lesnjak@gmail.com>
2025-01-23 14:21:54 +0100

    feat: added bluetooth

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 sudo pacman -Syu
-sudo pacman -Sy hyprland hyprpaper wofi nautilus alacritty waybar code chromium nerd-fonts starship p7zip jq neofetch pipewire pipewire-pulse pipewire-alsa wireplumber dolphin-emu nodejs npm intellij-idea-community-edition brightnessctl --noconfirm
+sudo pacman -Sy hyprland hyprpaper wofi nautilus alacritty waybar code chromium nerd-fonts starship p7zip jq neofetch pipewire pipewire-pulse pipewire-alsa wireplumber dolphin-emu nodejs npm intellij-idea-community-edition brightnessctl bluez bluez-utils --noconfirm
 
 cp -r ./config/* ~/.config
 chmod +x ./update.sh
@@ -14,3 +14,8 @@ chmod +x ~/.config/waybar/weather.sh
 sudo mkdir /usr/share/fonts/SFMonoFonts /usr/share/fonts/SFSymbols
 sudo cp ./fonts/SFMonoFonts/* /usr/share/fonts/SFMonoFonts
 sudo cp ./fonts/SFSymbols/* /usr/share/fonts/SFSymbols
+
+sudo systemctl start bluetooth.service
+sudo systemctl enable bluetooth.service
+
+#sudo vim /etc/bluetooth/main.conf and set AutoEnable to true
